5fa1526c84ba08a921e2396a93a1057c4411adf4
Lorence Lim <jlorencelim@gmail.com>
2018-12-07 06:58:22 +0800

    Add detail view

--- apps/tasks/views.py
+++ apps/tasks/views.py
@@ -50,6 +50,29 @@ class AvailableTaskListView(LoginRequiredMixin, ListView):
         return HttpResponseNotFound('<h1>Page not found</h1>')
 
 
+class TaskDetailView(LoginRequiredMixin, DetailView):
+    model = Task
+    template_name = 'tasks/task_detail.html'
+    pk_url_kwarg = 'pk'
+
+    def get_object(self):
+        return Task.objects.get(pk=self.kwargs['pk'])
+
+
+class AvailableTaskListView(LoginRequiredMixin, ListView):
+    model = Task
+    context_object_name = 'tasks'
+    template_name = 'tasks/available_task_list.html'
+
+    def get_queryset(self):
+        user = self.request.user
+
+        if user.type == user.TYPE_TASKER:
+            return Task.objects.filter(tasker=None)
+
+        return HttpResponseNotFound('<h1>Page not found</h1>')
+
+
 class TaskCreateView(LoginRequiredMixin, CreateView):
     model = Task
     form_class = TaskForm
